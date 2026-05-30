3f96b549f89b00425ef2af99bfb334d48d241df8
Vipin <vy879736@gmail.com>
2026-05-30 15:06:47 +0530

    chore(frontend):frontend dir set-up and liberaries installed. - index.html file imported from classRepo. - main.js also imported from classRepo. - style.css created and styling copied from classRepo. - uiButtons inside uiStuff created and selectors copied from classRepo.

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,60 +1,27 @@
+
+//Importing Dependencies
 import './style.css'
-import javascriptLogo from './assets/javascript.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import { setupCounter } from './counter.js'
+import buttons from "../uiStuff/uiButtons";
+import {Device} from 'mediasoup-client';
+import { io } from "socket.io-client";
 
-document.querySelector('#app').innerHTML = `
-<section id="center">
-  <div class="hero">
-    <img src="${heroImg}" class="base" width="170" height="179">
-    <img src="${javascriptLogo}" class="framework" alt="JavaScript logo"/>
-    <img src="${viteLogo}" class="vite" alt="Vite logo" />
-  </div>
-  <div>
-    <h1>Get started</h1>
-    <p>Edit <code>src/main.js</code> and save to test <code>HMR</code></p>
-  </div>
-  <button id="counter" type="button" class="counter"></button>
-</section>
 
-<div class="ticks"></div>
+//Using Dependencies
+const socket = io("http://localhost:3000");
+socket.on("connect",()=>{
+  console.log(`connected: ${socket.id}`);
+});
 
-<section id="next-steps">
-  <div id="docs">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#documentation-icon"></use></svg>
-    <h2>Documentation</h2>
-    <p>Your questions, answered</p>
-    <ul>
-      <li>
-        <a href="https://vite.dev/" target="_blank">
-          <img class="logo" src="${viteLogo}" alt="" />
-          Explore Vite
-        </a>
-      </li>
-      <li>
-        <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-          <img class="button-icon" src="${javascriptLogo}" alt="">
-          Learn more
-        </a>
-      </li>
-    </ul>
-  </div>
-  <div id="social">
-    <svg class="icon" role="presentation" aria-hidden="true"><use href="/icons.svg#social-icon"></use></svg>
-    <h2>Connect with us</h2>
-    <p>Join the Vite community</p>
-    <ul>
-      <li><a href="https://github.com/vitejs/vite" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#github-icon"></use></svg>GitHub</a></li>
-      <li><a href="https://chat.vite.dev/" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#discord-icon"></use></svg>Discord</a></li>
-      <li><a href="https://x.com/vite_js" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#x-icon"></use></svg>X.com</a></li>
-      <li><a href="https://bsky.app/profile/vite.dev" target="_blank"><svg class="button-icon" role="presentation" aria-hidden="true"><use href="/icons.svg#bluesky-icon"></use></svg>Bluesky</a></li>
-    </ul>
-  </div>
-</section>
 
-<div class="ticks"></div>
-<section id="spacer"></section>
-`
+console.log("main.js rendered");
 
-setupCounter(document.querySelector('#counter'))
+const joinRoom = ()=>{
+  console.log("room joined clicked");
+  const roomName = document .getElementById("room-input").value;
+  const userName = document.getElementById("username").value;
+  console.log("username: ",userName);
+  console.log("roomName: ",roomName);
+}
+
+buttons.joinRoom.addEventListener("click",joinRoom);
+
