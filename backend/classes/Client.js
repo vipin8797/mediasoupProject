506d83387bbaa2831935d2eb48e81a95bfa15b24
Vipin <vy879736@gmail.com>
2026-06-03 17:03:01 +0530

    fix(backend): return producer transport parameters
    Add client transport creation through the room router and return ICE, DTLS, and candidate parameters for frontend transport setup.
    
    Store producer and consumer transports on the connected client instance.
    
    Refs #1

--- backend/classes/Client.js
+++ backend/classes/Client.js
@@ -1,17 +1,62 @@
-class Client{
-    constructor(userName,socket){
-     this.userName = userName;
-     this.socket = socket;
+const config = require("../config/config");
 
-     this.upstreamTransport = null;
-     //we will have an audio and video consumer
-     this.producer = {};
+class Client {
+  constructor(userName, socket) {
+    this.userName = userName;
+    this.socket = socket;
 
-     this.downstreamTransport = [];
-     this.consumer = [];
-     this.room = null
+    this.upstreamTransport = null;
+    //we will have an audio and video consumer
+    this.producer = {};
 
-    }
+    this.downstreamTransport = [];
+    this.consumer = [];
+    this.room = null;
+  }
+  addTransport(type) {
+    return new Promise(async (resolve, reject) => {
+      const {
+        listenIps,
+        initialAvailabelOutgoingBiterate,
+        maxIncomingBitrate,
+      } = config.webRtcTransport;
+
+      //creating transport and router will get from Room object
+      const transport = await this.room.router.createWebRtcTransport({
+        listenIps,
+        initialAvailabelOutgoingBiterate,
+        enableUdp: true,
+        enableTcp: true,
+        preferUdp: true,
+      });
+
+      //setting Bandthwidth Limit
+      if (maxIncomingBitrate) {
+        try {
+          await transport.setMaxIncomingBitrate(maxIncomingBitrate);
+        } catch (err) {
+          console.log("Error setting Bitetarate:", err);
+        }
+      }
+
+      //storing transport according to type
+      if (type === "producer") {
+        this.upstreamTransport = transport;
+      } else if (type === "consumer") {
+        this.downstreamTransport = transport;
+      }
+
+      //sending params to client
+      const clientTransportParams = {
+        id: transport.id,
+        iceParameters: transport.iceParameters,
+        iceCandidates: transport.iceCandidates,
+        dtlsParameters: transport.dtlsParameters,
+      };
+
+      resolve(clientTransportParams); //sending params
+    });
+  }
 }
 
 module.exports = Client;
