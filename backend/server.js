cdb811bdcc18b9e22a009041ee8aeea690af9863
Vipin <vy879736@gmail.com>
2026-06-04 14:09:56 +0530

    feat(signaling): handle producer transport lifecycle
    Add server-side handlers for producer transport connection and producer creation.
    
    Acknowledge successful DTLS connection with the client-expected ok response so the send transport can complete its connect callback.
    
    Create producers from the connected client's upstream transport and return the mediasoup producer id to the browser.
    
    Normalize server formatting around the socket connection flow while preserving the existing room and transport creation behavior.

--- backend/server.js
+++ backend/server.js
@@ -1,12 +1,11 @@
 //Requiring Dependencies
 const express = require("express");
-const socket  = require("socket.io");
+const socket = require("socket.io");
 const mediasoup = require("mediasoup");
 const path = require("path");
 const http = require("http");
 const fs = require("fs");
 const config = require("./config/config");
-
 
 const createWorkers = require("./utilities/createWorker");
 const Client = require("./classes/Client");
@@ -17,60 +16,48 @@ const { generateRouterRtpCapabilities } = require("mediasoup/ortc");
 //Using Dependencies
 const app = express();
 const httpServer = http.createServer(app);
-const io = socket(httpServer,{
-    cors:{
-        origin:`http://localhost:5173`,
-    }
-})
-
+const io = socket(httpServer, {
+  cors: {
+    origin: `http://localhost:5173`,
+  },
+});
 
 //Global Variables
 let workers = [];
 let router = null;
 let rooms = []; //array to store all activve rooms in it.
 
-
 //Middlewares
 
+const initMediasoup = async () => {
+  //Creating Worker
+  workers = await createWorkers(); //arraya of total workers
+};
 
-
-const initMediasoup =  async()=>{    
-    //Creating Worker
-    workers = await createWorkers(); //arraya of total workers
-
-    }
-
-
-initMediasoup();//creating worker and router
-
-
+initMediasoup(); //creating worker and router
 
 //Socket events created to listen for clinet
-io.on("connection",(socket)=>{
-    let newRoom = false;
-    console.log("connected: ",socket.id);
-    let client ; //this client obj will be available to all socket listners
-    const handshake = socket.handshake; //where auth token and query lives
+io.on("connection", (socket) => {
+  let newRoom = false;
+  console.log("connected: ", socket.id);
+  let client; //this client obj will be available to all socket listners
+  const handshake = socket.handshake; //where auth token and query lives
 
+  //socket connect event
+  socket.on("joinRoom", async ({ userName, roomName }, ac) => {
+    try {
+      client = new Client(userName, socket); //creating client obj from class
 
-//socket connect event
-    socket.on("joinRoom",async({userName,roomName},ac)=>{
-        try{
-
-        
-      client = new Client(userName,socket); //creating client obj from class
-      
-      let requestedRoom = rooms.find(room=>room.roomName == roomName);
-      if(!requestedRoom){
+      let requestedRoom = rooms.find((room) => room.roomName == roomName);
+      if (!requestedRoom) {
         newRoom = true;
         //make a new room
-        const workerToUse = await getWorker(workers); 
-        requestedRoom = new Room(roomName,workerToUse); //creating room objc
-         await requestedRoom.createRouter();  //creating router for room
-         rooms.push(requestedRoom);//pushing room to master rooms array
+        const workerToUse = await getWorker(workers);
+        requestedRoom = new Room(roomName, workerToUse); //creating room objc
+        await requestedRoom.createRouter(); //creating router for room
+        rooms.push(requestedRoom); //pushing room to master rooms array
+      }
 
-      }   
-      
       //add room to the Client obj
       client.room = requestedRoom;
       //add Client in the Room objc
@@ -79,53 +66,66 @@ io.on("connection",(socket)=>{
       //add this socket to the socket room
       socket.join(client.room.roomName);
 
-      ac(
-        {
-            routerRtpCapabilities : client.room.router.rtpCapabilities,
-            newRoom,
-        }
-      )
-    
-    }catch(err){
-        console.warn(err);
-
+      ac({
+        routerRtpCapabilities: client.room.router.rtpCapabilities,
+        newRoom,
+      });
+    } catch (err) {
+      console.warn(err);
     }
-    });
-
-
+  });
 
   //Event to create Transport both Producer and Consumer
-  socket.on("request-transport",async({type},ack)=>{
-    let clientTranportParams = null;  //params to sent to client
+  socket.on("request-transport", async ({ type }, ack) => {
+    let clientTranportParams = null; //params to sent to client
 
-    if(type === 'producer'){
-         clientTranportParams = await client.addTransport(type);
-        }else if(type === "consumer"){
-        clientTranportParams = await client.addTransport(type);
+    if (type === "producer") {
+      clientTranportParams = await client.addTransport(type);
+    } else if (type === "consumer") {
+      clientTranportParams = await client.addTransport(type);
     }
 
+    console.log("transport created");
     ack(clientTranportParams);
+  });
 
-  })  
-    
-    
+  //ProducerTransport connect event
+  socket.on("connect-transport", async ({ dtlsParameters, type }, ack) => {
+    try {
+      if (type === "producer") {
+        await client.upstreamTransport.connect({ dtlsParameters });
+        ack("ok");
+      } else if (type === "consumer") {
+        // TODO: connect downstream transport
+      }
+      console.log("connect.....");
+    } catch (err) {
+      console.log(err);
+      ack("error");
+    }
+  });
 
-})//socket end
 
+  //ProducerTransport produce event
+  socket.on("start-producing", async ({ kind, rtpParameters }, ack) => {
+    try {
+        console.log("produce event start");
+      const newProducer = await client.upstreamTransport.produce({
+        kind,
+        rtpParameters,
+      });
 
-
-
-
+      //TODO: if audio track is avalaible then it is possible that it may be Dominant speaker in Room objc
+      //TODO: There are other memebers in Room ,notify them
+      console.log("produce end.....")
+      ack(newProducer.id);
+    } catch (err) {
+      ack("error");
+    }
+  });
+}); //socket end
 
 //Server
 httpServer.listen(config.port, () => {
-    console.log(`listening at ${config.port}`);
-});        
-        
-        
-        
-        
-        
-        
-        
-    
+  console.log(`listening at ${config.port}`);
+});
